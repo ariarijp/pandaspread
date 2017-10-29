ae32b1787ff8a70d51d2ec5a6a08d5597ee34346
Takuya Arita <arita@cocolable.co.jp>
2017-10-29 13:31:35 +0900

    Add required_scopes function

--- pandaspread/__init__.py
+++ pandaspread/__init__.py
@@ -2,6 +2,10 @@ import pandas as pd
 from googleapiclient.discovery import Resource
 
 
+def required_scopes() -> list:
+    return ['https://www.googleapis.com/auth/spreadsheets']
+
+
 def write_to_spreadsheet(service: Resource, spreadsheet_id: str, spreadsheet_range: str, df: pd.DataFrame, **kwargs) -> dict:
     value_input_option = kwargs.get('value_input_option', 'USER_ENTERED')
     values = [df.columns.values.tolist()] + df.fillna('').values.tolist()
