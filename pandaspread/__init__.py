0fff06facbb81435b57d467fa6268b1fd2aa8680
Takuya Arita <arita@cocolable.co.jp>
2017-10-29 13:28:50 +0900

    Add Type hint

--- pandaspread/__init__.py
+++ pandaspread/__init__.py
@@ -2,7 +2,7 @@ import pandas as pd
 from googleapiclient.discovery import Resource
 
 
-def write_to_spreadsheet(service: Resource, spreadsheet_id: str, spreadsheet_range: str, df: pd.DataFrame, **kwargs):
+def write_to_spreadsheet(service: Resource, spreadsheet_id: str, spreadsheet_range: str, df: pd.DataFrame, **kwargs) -> dict:
     value_input_option = kwargs.get('value_input_option', 'USER_ENTERED')
     values = [df.columns.values.tolist()] + df.fillna('').values.tolist()
     body = {
